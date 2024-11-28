8fc144671c8cb0b9369f8fe340616556f430fe5d
6704062617211 <s6704062617211@email.kmutnb.ac.th>
2024-11-28 18:17:19 +0700

    4 - input again

--- lab1minx.cpp
+++ lab1minx.cpp
@@ -2,8 +2,8 @@
 int main() {
 	int n1;
 	int n2;
-	printf("Type n1&n2: \n");
-	scanf("%d %d" , &n1 , &n2);
+	scanf("%d" , &n1);
+	scanf("%d" , &n2);
 
 	if(n1>n2){
 		printf("n1 is greater than n2");
